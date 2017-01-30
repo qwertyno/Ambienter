1772860127fb852281cdd6f820f3e1b623a2d2c7
Virak X64 <virakx64@qwerty.no>
2017-01-30 18:43:43 +0100

    Replaced larger icon

--- Source/Qwerty.Vsix.Ambienter/source.extension.cs
+++ Source/Qwerty.Vsix.Ambienter/source.extension.cs
@@ -8,10 +8,10 @@ namespace Qwerty.Vsix.Ambienter
     static class Vsix
     {
         public const string Id = "492371a3-b83c-450e-b48f-18f766cdb375";
-        public const string Name = "Qwerty.Ambienter";
+        public const string Name = "Ambienter";
         public const string Description = @"Visual Studio Extention for setting up ambient environment for a project.";
         public const string Language = "en-US";
-        public const string Version = "0.1.0.1";
+        public const string Version = "0.1.0.2";
         public const string Author = "Qwerty";
         public const string Tags = "";
     }
